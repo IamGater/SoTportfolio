a86a79468882bea072b4058aa7d78752cb8f00aa
IamGater <116513307+IamGater@users.noreply.github.com>
2023-11-16 19:46:22 +0200

    burger

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector(".burger");
+const list = document.querySelector('.header__list')
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
+  list.classList.toggle('active')
 });
-
