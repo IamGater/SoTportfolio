0f4e6392c49f336f9ca53b719379e608af9d5263
IamGater <116513307+IamGater@users.noreply.github.com>
2023-11-18 09:40:21 +0200

    fix burger

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const burger = document.querySelector(".burger");
-const list = document.querySelector('.header__list')
+const list = document.querySelector(".header__list");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
-  list.classList.toggle('active')
+  list.classList.toggle("active");
 });
